41a3ae27790bca12e0a330c802b0d75fdf93d94b
Randi Mikkelsen Bech <au615785@uni.au.dk>
2020-03-11 15:44:41 +0100

    Display og IDisplay

---
+++ Ladeskab/Ladeskab/Display.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ladeskab
+{
+    public class Display : IDisplay
+    {
+
+        public void showChargeMsg(string chargeMessage)
+        {
+            Console.WriteLine(chargeMessage);
+        }
+        public void showStationMsg(string stationMessage)
+        {
+            Console.WriteLine(stationMessage);
+        }
+    }
+}
---
+++ Ladeskab/Ladeskab/IDisplay.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ladeskab
+{
+    public interface IDisplay
+    {
+        void showChargeMsg(string chargeMessage);
+        void showStationMsg(string stationMessage);
+    }
+}
