3cfcf5737708efdd8869f3e8c3e236b6f9a060aa
Randi Mikkelsen Bech <au615785@uni.au.dk>
2020-03-11 16:04:31 +0100

    Logfile added

---
+++ Ladeskab/Ladeskab/ILogFile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ladeskab
+{
+    public interface ILogFile
+    {
+        void logDoorLock(int id);
+        void logDoorUnlocked(int id);
+    }
+}
