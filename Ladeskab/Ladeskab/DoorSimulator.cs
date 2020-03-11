2902c351932c034ebb1c03458387c6cf936732b2
Jonas Bay Andreasen <au616208@uni.au.dk>
2020-03-11 15:57:39 +0100

    Door finished

---
+++ Ladeskab/Ladeskab/DoorSimulator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ladeskab
+{
+    public class DoorSimulator : IDoor
+    {
+        private bool IsTheDoorOpen;
+        private bool doorLocked;
+
+        public event EventHandler<doorOpenEvent> doorOpen;
+        public event EventHandler<doorCloseEvent> doorClose;
+
+        protected virtual void OnDoorOpen(doorOpenEvent e)
+        {
+            if (doorLocked)
+            {
+                Console.Write("Door cannot open! Its locked.");
+            }
+            else
+            {
+                IsTheDoorOpen = true;
+            }
+        }
+
+        protected virtual void OnDoorClose(doorCloseEvent e)
+        {
+            if (IsTheDoorOpen)
+            {
+                IsTheDoorOpen = false;
+            }
+            else
+            {
+                Console.Write("Door is not open!");
+            }
+        }
+
+        public bool unlockDoor()
+        {
+            if (IsTheDoorOpen == true)
+            {
+                Console.WriteLine("Door is already open and cannot be unlocked.");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public bool lockDoor()
+        {
+            if (IsTheDoorOpen == false)
+            {
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Door is already open and cannot be locked.");
+                return false;
+            }
+        }
+    }
+}
