c381c07cbd8cc25884872736226b617effc7ea8f
Jonas Bay Andreasen <au616208@uni.au.dk>
2020-03-17 09:57:49 +0100

    Jonas

---
+++ Ladeskab/LadeskabUnitTest/TestDoorSimulator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+using DoorSimulator = Ladeskab.DoorSimulator;
+
+namespace Ladeskab
+{
+    [TestClass]
+    public class TestDoorSimulator
+    {
+        private DoorSimulator _doorSimulator;
+        [SetUp]
+        public void Setup()
+        {
+            _doorSimulator = new DoorSimulator();
+        }
+
+        [Test]
+        public void TestMethod1()
+        {
+            _doorSimulator.DoorLockStatus = false;
+            
+            _doorSimulator.OnDoorOpen();
+
+            Assert.That(_doorSimulator.DoorStatus, Is.True);
+        }
+    }
+}
