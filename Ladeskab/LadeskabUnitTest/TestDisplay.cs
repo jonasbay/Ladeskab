a9eb78775ec6af19c2259d6c99599b23fbf50afb
Randi Mikkelsen Bech <au615785@uni.au.dk>
2020-03-18 16:12:40 +0100

    TestDisplay

--- Ladeskab/LadeskabUnitTest/TestDisplay.cs
+++ Ladeskab/LadeskabUnitTest/TestDisplay.cs
@@ -5,18 +5,34 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NUnit.Framework;
+using Ladeskab;
+using NSubstitute;
 
 namespace LadeskabUnitTest
 {
     [TestFixture]
     public class TestDisplay
     {
+        private IDisplay _uut;
         private IConsoleWriteLine _consoleWriteLine;
+
         [SetUp]
         public void setup()
         {
-
+            _consoleWriteLine = Substitute.For<IConsoleWriteLine>();
+            _uut = new Display(_consoleWriteLine);
+  
         }
+
+
+        //se https://nsubstitute.github.io/help/received-calls/ for mere hjælp!
+        [Test]
+        public void DidWriteLineGetCall()
+        {
+            _consoleWriteLine.DidNotReceive().writeLine();
+        }
+
+
 
     }
 }
